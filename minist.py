1f6b0ddfa80cdcb9b625e14ef7eb63d9b3ab1618
ZYF <m13110683916@163.com>
2022-05-15 20:47:05 +0800

    minist.py 的一些调整

--- minist.py
+++ minist.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import torch.backends.cudnn as cudnn
+import torch.utils.data
 from torchvision import datasets, transforms
 
 
@@ -16,7 +18,7 @@ class Network(nn.Module):
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
-        x = F.max_pool2d(x, 2, 2)  # kernel_size=2, stride=2，pooling之后的大小除以2
+        x = F.max_pool2d(x, 2, 2)  # 2*2的核，步长为2，pooling之后的大小除以2
 
         x = F.relu(self.conv2(x))
         x = F.max_pool2d(x, 2, 2)
@@ -24,8 +26,8 @@ class Network(nn.Module):
         x = x.view(-1, 40 * 4 * 4)  # 展开为行向量
 
         x = F.relu(self.fc1(x))
+        x = F.dropout(input=x, p=0.5, training=self.training)  # 弃权， 一半的神经元
 
-        x = F.dropout(input=x, p=0.5, training=self.training)
         x = self.fc2(x)
         x = F.log_softmax(x, dim=1)  # 按行进行log(softmax(x))
         return x
@@ -45,28 +47,28 @@ def data_loader(batch_size, batch_size_test, use_cuda=False):
     # GPU训练需要的参数
     kwargs = {'num_workers': 4, 'pin_memory': True} if use_cuda else {}
 
+    # 数据处理器
+    transform = transforms.Compose([
+        # 把[0,255]的(H,W,C)的图片转换为[0,1]的(channel,height,width)的图片
+        transforms.ToTensor(),
+        # z-score标准化为标准正态分布
+        # 这两个数分别是MNIST的均值和标准差
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])
+
     train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST(root='./files/',
+        datasets.MNIST(root='./data/',
                        train=True,
                        download=True,
-                       transform=transforms.Compose([
-                           # 把[0,255]的(H,W,C)的图片转换为[0,1]的(channel,height,width)的图片
-                           transforms.ToTensor(),
-                           # z-score标准化为标准正态分布
-                           # 这两个数分别是MNIST的均值和标准差
-                           transforms.Normalize((0.1307,), (0.3081,))
-                       ])),
+                       transform=transform),
         batch_size=batch_size,
         shuffle=True,
         **kwargs)
 
     test_loader = torch.utils.data.DataLoader(
-        datasets.MNIST(root='./files/',
+        datasets.MNIST(root='./data/',
                        train=False,
-                       transform=transforms.Compose([
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
-                       ])),
+                       transform=transform),
         batch_size=batch_size_test,
         shuffle=True,
         **kwargs)
@@ -111,15 +113,16 @@ def train(log_interval, network, device, train_loader, batch_size_train, train_l
             train_losses.append(loss.item())
             train_counter.append(
                 (batch_idx * batch_size_train) + ((epoch - 1) * len(train_loader.dataset)))
-            torch.save(network.state_dict(), 'results/model.pth')
-            torch.save(optimizer.state_dict(), 'results/optimizer.pth')
+            # torch.save(network.state_dict(), 'results/model.pth')
+            # torch.save(optimizer.state_dict(), 'results/optimizer.pth')
 
 
 # 测试
-def test(network, device, test_loader, test_losses):
+def test(network, device, test_loader, test_losses, accuracies=None):
     """
     测试过程
 
+    :param accuracies: 记录每次迭代的精确度
     :param network: 网络模型
     :param device: 测试使用的设备
     :param test_loader: 测试集
@@ -141,9 +144,11 @@ def test(network, device, test_loader, test_losses):
     test_loss /= len(test_loader.dataset)
     test_losses.append(test_loss)
 
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
+
+    accuracies.append(100. * correct / len(test_loader.dataset))
 
 
 def drawFig(train_counter, train_losses, test_counter, test_losses):
@@ -159,8 +164,8 @@ def drawFig(train_counter, train_losses, test_counter, test_losses):
     import matplotlib.pyplot as plt
 
     plt.figure()
-    plt.plot(train_counter, train_losses, color='blue')
-    plt.scatter(test_counter, test_losses, color='red')
+    plt.plot(train_counter[:len(train_losses)], train_losses, color='blue')
+    plt.scatter(test_counter[:len(test_losses)], test_losses, color='red')
     plt.legend(['Train Loss', 'Test Loss'], loc='upper right')
     plt.xlabel('number of training examples seen')
     plt.ylabel('negative log likelihood loss')
@@ -168,7 +173,7 @@ def drawFig(train_counter, train_losses, test_counter, test_losses):
 
 
 if __name__ == '__main__':
-    n_epochs = 3
+    n_epochs = 20
     batch_size_train = 64
     batch_size_test = 1000
     learning_rate = 0.01
@@ -192,33 +197,42 @@ if __name__ == '__main__':
     train_counter = []
     test_losses = []
     test_counter = [i * len(train_loader.dataset) for i in range(n_epochs + 1)]
+    accuracies = []
+    max_acc = 0.
+    max_interval = 3
 
     network = Network().to(device)
     optimizer = optim.SGD(network.parameters(), lr=learning_rate, momentum=momentum)
 
-    test(network, device, test_loader, test_losses)
+    test(network, device, test_loader, test_losses, accuracies)
+    max_acc = max(max_acc, accuracies[-1])
     for epoch in range(1, n_epochs + 1):
         train(log_interval, network, device, train_loader, batch_size_train, train_losses, train_counter, optimizer,
               epoch)
-        test(network, device, test_loader, test_losses)
+        test(network, device, test_loader, test_losses, accuracies)
+
+        max_acc = max(max_acc, accuracies[-1])
+        print('Max accuracy: {:.2f}%\n'.format(max_acc))
+        if max(accuracies[-max_interval:]) < max_acc:
+            print('No progress, stop training.')
+            break
 
     drawFig(train_counter, train_losses, test_counter, test_losses)
 
-    if input('Continue training?(y/n)') == 'y':
-        continued_network = Network()
-        continued_optimizer = optim.SGD(network.parameters(), lr=learning_rate,
-                                        momentum=momentum)
-        network_state_dict = torch.load('results/model.pth')
-        continued_network.load_state_dict(network_state_dict)
-
-        optimizer_state_dict = torch.load('results/optimizer.pth')
-        continued_optimizer.load_state_dict(optimizer_state_dict)
-
-        for epoch in range(4, 9):
-            test_counter.append(epoch * len(train_loader.dataset))
-            train(log_interval, network, device, train_loader, batch_size_train, train_losses, train_counter, optimizer,
-                  epoch)
-            test(network, device, test_loader, test_losses)
-
-        drawFig(train_counter, train_losses, test_counter, test_losses)
-
+    # if input('Continue training?(y/n)') == 'y':
+    #     continued_network = Network()
+    #     continued_optimizer = optim.SGD(network.parameters(), lr=learning_rate,
+    #                                     momentum=momentum)
+    #     network_state_dict = torch.load('results/model.pth')
+    #     continued_network.load_state_dict(network_state_dict)
+    #
+    #     optimizer_state_dict = torch.load('results/optimizer.pth')
+    #     continued_optimizer.load_state_dict(optimizer_state_dict)
+    #
+    #     for epoch in range(4, 9):
+    #         test_counter.append(epoch * len(train_loader.dataset))
+    #         train(log_interval, network, device, train_loader, batch_size_train, train_losses, train_counter, optimizer,
+    #               epoch)
+    #         test(network, device, test_loader, test_losses)
+    #
+    #     drawFig(train_counter, train_losses, test_counter, test_losses)
